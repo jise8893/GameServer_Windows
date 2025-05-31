02e86869934a5678a3753429d8206a7d9bc95a79
mason <mason@MASON13A1>
2025-05-31 21:12:06 +0900

    add: ReadLockGuard, WriteLockGuard 구현

--- Core/ReaderWriterLock.h
+++ Core/ReaderWriterLock.h
@@ -1,6 +1,12 @@
 #pragma once
 
 #include <atomic>
+/// 
+/// @class Lock (SpinLock)
+/// 
+/// @brief 데이터의 참조의 경우 다른 스레드의 데이터 참조는 허용하고,
+/// 데이터 변경에 대한 동기화 작업 시, 동기화가 필요한 데이터에 대한 작업을 대기시킨다.
+///
 class Lock
 {
 	enum : uint32_t
@@ -22,15 +28,22 @@ private:
 	uint16_t m_writeCount = 0; // Write 에 대해 중첩 락을 허용하기 때문에 RAII Unlock시, 시도 횟수를 계산한다.
 
 };
-/// 
-/// @class ReaderWriterLock (SpinLock)
-/// 
-/// @brief 데이터의 참조의 경우 다른 스레드의 데이터 참조는 허용하고,
-/// 데이터 변경에 대한 동기화 작업 시, 동기화가 필요한 데이터에 대한 작업을 대기시킨다.
-///
-class ReaderWriterLock
+
+class ReadLockGuard
 {
-
-
+public:
+	ReadLockGuard(Lock& lock) : m_lock(lock) { m_lock.ReadLock(); }
+	~ReadLockGuard() { m_lock.ReadUnlock(); }
+private:
+	Lock& m_lock;
 };
 
+
+class WriteLockGuard
+{
+public:
+	WriteLockGuard(Lock& lock) : m_lock(lock) { m_lock.WriteLock(); }
+	~WriteLockGuard() { m_lock.WriteUnlock(); }
+private:
+	Lock& m_lock;
+};
